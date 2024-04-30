c255f28fc46dad29db8c9e64335b622d2a8afe58
Vinay Chouhan <chouhanvinay638@gmail.com>
2024-04-30 17:55:08 +0530

    update projects section

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Title from '../layouts/Title'
-import { projectOne, projectTwo, projectThree } from "../../assets/index";
+import { projectOne, projectTwo, projectThree, projectFour, projectFive, projectSix } from "../../assets/index";
 import ProjectsCard from './ProjectsCard';
 
 const Projects = () => {
@@ -17,40 +17,39 @@ const Projects = () => {
       </div>
       <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6 xl:gap-14">
         <ProjectsCard
-          title="SOCIAL MEDIA CLONE"
-          des=" Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-              Explicabo quibusdam voluptate sapiente voluptatibus harum quidem!"
+          title="PETSTABLISHED"
+          des="Petstablished is a software to Animal Welfare Organizations. 
+          AWOs can track information for all of their pets, adopters, fosters, donations, and staff."
           src={projectOne}
         />
         <ProjectsCard
-          title="E-commerce Website"
-          des=" Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-              Explicabo quibusdam voluptate sapiente voluptatibus harum quidem!"
+          title="RISKCOVRY"
+          des="Riskcovry is an Insurtech or a Digital Insurance Company 
+          based out of India that enables insurance distribution for enterprise customers."
           src={projectTwo}
         />
         <ProjectsCard
-          title="Chatting App"
-          des=" Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-              Explicabo quibusdam voluptate sapiente voluptatibus harum quidem!"
+          title="DISPATCH TRACK"
+          des="DispatchTrack is the global leader in last mile 
+          delivery solutions, helping distribution companies around the world power successful deliveries 1 million times a day."
           src={projectThree}
         />
         <ProjectsCard
-          title="SOCIAL MEDIA CLONE"
-          des=" Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-              Explicabo quibusdam voluptate sapiente voluptatibus harum quidem!"
-          src={projectThree}
+          title="PAYVMNT"
+          des="Real-time route optimization software crowdsourced from drivers to provide drivers a better delivery experience. Directed to your specific drop location."
+          src={projectFour}
         />
         <ProjectsCard
-          title="E-commerce Website"
-          des=" Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-              Explicabo quibusdam voluptate sapiente voluptatibus harum quidem!"
-          src={projectOne}
+          title="TUNEFRUIT"
+          des="Tunefruit is your hip, cool friend that always knows where to find the 
+          freshest production music for your explainer video. License Music. Simply."
+          src={projectFive}
         />
         <ProjectsCard
-          title="Chatting App"
-          des=" Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-              Explicabo quibusdam voluptate sapiente voluptatibus harum quidem!"
-          src={projectTwo}
+          title="PRINT SEC"
+          des="PrintSEC is a SaaS-based encrypted printing service that offers a secure and simple solution, 
+          It does not require any additional hardware and software infrastructure"
+          src={projectSix}
         />
       </div>
     </section>
